6b9dd8f4e3b128eb64ca4692df95c870c19e6a07
Kate McFaul <katemcfaul@gmail.com>
2018-02-06 09:32:19 -0800

    refactor completedchores.js

--- src/Components/CompletedChores/CompletedChores.js
+++ src/Components/CompletedChores/CompletedChores.js
@@ -3,81 +3,70 @@ import React from 'react';
 const CompletedChores = (props) => {
   // Default values
   const noCompleted = <tr><td colSpan="2" style={{fontWeight: "normal"}}>No completed chores</td></tr>;
+  let jackChores = noCompleted;
+  let nobyChores = noCompleted;
   const noRequired = <tr><td colSpan="2" style={{fontWeight: "normal"}}>No required chores :)</td></tr>;
+  let jackRequiredChores = noRequired;
+  let nobyRequiredChores = noRequired;
   let jackPoints = 0;
   let nobyPoints = 0;
 
+  // fetch and return chores
+  const cycleThroughCompleted = (kid, required) => {
+    let kidChores;
+    let kidPoints = 0;
+    let chores;
+    if (kid === 'jack' && required === false) chores = props.jack;
+    if (kid === 'noby' && required === false) chores = props.noby;
+    if (kid === 'jack' && required === true) chores = props.jackRequired;
+    if (kid === 'noby' && required === true) chores = props.nobyRequired;
+    if ((kid === 'jack' && chores) || (kid === 'noby' && chores)) {
+      kidChores = Object.keys(chores).map(key => {
+        const chore = chores[key];
+        if (required) {
+          const note = chore.notes ? <span className="notes"><br />{chore.notes}</span> : null;
+          const initial = kid === 'jack' ? "J" : "N";
+          let doneStyle = 'bold';
+          let doneButton = <a onClick={() => props.toggleRequiredChore(kid, key)} className={kid} >{initial}</a>
+          if (chore.completed) {
+            doneStyle = 'normal';
+            doneButton = <a onClick={() => props.toggleRequiredChore(kid, key)}>undo</a>
+          }
+          return (
+            <tr key={key}>
+              <td style={{ fontWeight: doneStyle }}>{chore.name} <span><a onClick={() => props.deleteRequired(kid, key)}>(x)</a></span> {note}</td>
+              <td className="doneColumn">{doneButton}</td>
+            </tr>
+          )
+        } else {
+          kidPoints += chore.points
+          return (
+            <tr key={key}>
+              <td>{chore.name} <span><a onClick={() => props.putBack(kid, key, chore)}>put back</a></span></td>
+              <td>{chore.points}</td>
+            </tr>
+          )
+        }
+        
+      })
+    }
+    if (kid === 'jack') {
+      jackPoints = kidPoints;
+      return kidChores;
+    } else if (kid === 'noby') {
+      nobyPoints = kidPoints;
+      return kidChores;
+    }
+  }
+
   // Cycle through Jack's completed chores
-  let jackChores = noCompleted;
-  if (props.jack) {
-    jackChores = Object.keys(props.jack).map(key => {
-      const chore = props.jack[key];
-      jackPoints += chore.points;
-      return (
-        <tr key={key}>
-          <td>{chore.name} <span><a onClick={() => props.putBack('jack', key, chore)}>put back</a></span></td>
-          <td>{chore.points}</td>
-        </tr>
-      )
-    })
-  }
-
-  // Cycle through Jack's required chores
-  let jackRequiredChores = noRequired;
-  if (props.jackRequired) {
-    jackRequiredChores = Object.keys(props.jackRequired).map(key => {
-      const chore = props.jackRequired[key];
-      const note = chore.notes ? <span className="notes"><br />{chore.notes}</span> : null;
-      let doneStyle = 'bold';
-      let doneButton = <a onClick={() => props.toggleRequiredChore('jack', key)} className="jack" >J</a>
-      if (chore.completed) {
-        doneStyle = 'normal';
-        doneButton = <a onClick={() => props.toggleRequiredChore('jack', key)}>undo</a>
-      }
-      return (
-        <tr key={key}>
-          <td style={{ fontWeight: doneStyle }}>{chore.name} <span><a onClick={() => props.deleteRequired('jack', key)}>(x)</a></span> {note}</td>
-          <td className="doneColumn">{doneButton}</td>
-        </tr>
-      )
-    })
-  }
-  
+  jackChores = cycleThroughCompleted('jack', false);
   // Cycle through Noby's completed chores
-  let nobyChores = noCompleted;  
-  if (props.noby) {
-    nobyChores = Object.keys(props.noby).map(key => {
-      const chore = props.noby[key];
-      nobyPoints += chore.points;
-      return (
-        <tr key={key}>
-          <td>{chore.name} <span><a onClick={() => props.putBack('noby', key, chore)}>put back</a></span></td>
-          <td>{chore.points}</td>
-        </tr>
-      )
-    })
-  }
-
-  // Cycle through Noby's required chores
-  let nobyRequiredChores = noRequired;
-  if (props.nobyRequired) {
-    nobyRequiredChores = Object.keys(props.nobyRequired).map(key => {
-      const chore = props.nobyRequired[key];
-      const note = chore.notes ? <span className="notes"><br />{chore.notes}</span> : null;
-      let doneStyle = 'bold';
-      let doneButton = <a onClick={() => props.toggleRequiredChore('noby', key)} className="noby" >N</a>
-      if (chore.completed) {
-        doneStyle = 'normal';
-        doneButton = <a onClick={() => props.toggleRequiredChore('noby', key)}>undo</a>
-      }
-      return (
-        <tr key={key}>
-          <td style={{ fontWeight: doneStyle }}>{chore.name} <span><a onClick={() => props.deleteRequired('noby', key)}>(x)</a></span> {note}</td>
-          <td className="doneColumn">{doneButton}</td>
-        </tr>
-      )
-    })
-  }
+  nobyChores = cycleThroughCompleted('noby', false);
+  // Cycle through Jack's required chores
+  jackRequiredChores = cycleThroughCompleted('jack', true);
+  // Cycle through Jack's required chores
+  nobyRequiredChores = cycleThroughCompleted('noby', true);
 
   return (
     <React.Fragment>
